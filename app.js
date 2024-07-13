d5440f728b2811677d21d3f431b058b554c1750d
Arpit12pandey <arpitpandeyjpr@gmail.com>
2024-07-13 23:13:06 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const userRouter = require('./routes/user');
+const dotenv =require('dotenv');
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log('connected to database successfully!!!');
+}).catch((err)=>{
+    console.log(`cannot connect to database:${err}`);
+});
+//'/users' is the mount path. This means that any requests to routes that start with /users will be handled by the userRouter middleware.
+
+// Routes
+app.use('/users', userRouter);
+
+module.exports = app;
